80b360c34fe74e3e1da9f10ebc1b6ee13cee5210
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-08-13 23:03:19 +0100

    meisam.backend.Search By Title and Username
    I add Search By Title on questions DB and if we want to search by Title its work then I add search by Username on Users List to see all the quctions by that user

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -16,18 +16,37 @@ router.route("/add").post((req, res) => {
     const answers = req.body.answers;
 
 
-const newQuestion = new Question({
-    username,
-    title,
-    question,
-    date,
-    module,
-    answers,
-});
+    const newQuestion = new Question({
+        username,
+        title,
+        question,
+        date,
+        module,
+        answers,
+    });
 
-newQuestion.save()
-.then(() => res.json("Question added!"))
-.catch(err => res.status(400).json("Error:" + err));
+    newQuestion.save()
+    .then(() => res.json("Question added!"))
+    .catch(err => res.status(400).json("Error:" + err));
+});
+// search by title of question 
+router.route("/:questionsTitle").get((req, res) => {
+    const title = req.params.questionsTitle;
+    Question.find({title: title})
+    .select()
+    .exec()
+    .then(titleFind =>{
+        console.log("Form database ", titleFind);
+        if (titleFind) {
+            res.status(200).json(titleFind);
+        } else {
+            res.status(400).json("No valid entry found for Title :" + err)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err });
+    });
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,4 +19,24 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error:" + err))
 });
 
+// search by UserName of question 
+router.route("/:username").get((req, res) => {
+    const username = req.params.username;
+    User.find({username: username})
+    .select()
+    .exec()
+    .then(usernameFind =>{
+        console.log("Form database ", usernameFind);
+        if (usernameFind) {
+            res.status(200).json(usernameFind);
+        } else {
+            res.status(400).json("No valid entry found for Title :" + err)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err });
+    });
+});
+
 module.exports = router;
