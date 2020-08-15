6ac4ddc1857cc90a27d7236a1f4628a24e033081
ellie <ellie.tms1371@gmail.com>
2020-08-15 22:43:01 +0100

    Final backEnd code for review

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,70 +1,74 @@
-
 const router = require("express").Router();
 const Question = require("../models/question.model");
 const Answer = require("../models/answer.model");
 
 // all the questions:
-router.route('/').get((req, res) => {
-	Question.find().populate('answers').select('answers').then((Find, err) => {
-		console.log('Form database ', Find);
-		if (Find) {
-			res.status(200).json(Find);
-		} else {
-			res.status(400).json('No valid entry found for Data :' + err);
-		}
-	});
+router.route("/").get((req, res) => {
+  Question.find()
+    .populate("answers")
+    .select("answers")
+    .then((Find, err) => {
+      console.log("Form database ", Find);
+      if (Find) {
+        res.status(200).send(Find);
+      } else {
+        res.status(400).send("No valid entry found for Data :" + err);
+      }
+    });
 });
 
 //create new Question :
-router.route('/ask').post((req, res) => {
-	const newQuestion = new Question(req.body.question);
-	console.log(newQuestion);
-	newQuestion.save((err, question) => {
-		if (err) {
-			return res.status(500).json({ 'something is wrong': err });
-		} else {
-			return res.status(201).json(question);
-		}
-	});
+router.route("/ask").post((req, res) => {
+  const newQuestion = new Question(req.body.question);
+  console.log(newQuestion);
+  newQuestion.save((err, question) => {
+    if (err) {
+      return res.status(500).json({ "something is wrong": err });
+    } else {
+      return res.status(201).json(question);
+    }
+  });
+});
+
+// Get Question By ID
+router.route("/:questionId").get((req, res) => {
+  const questionId = req.params.questionId;
+  console.log(questionId);
+  Question.findById(req.params.questionId, (err, questionWithId) => {
+    console.log(questionWithId);
+    console.log("Form database ", questionWithId);
+    if (questionWithId) {
+      res.status(200).json(questionWithId);
+    } else {
+      res.status(400).json("No valid entry found for ID ");
+    }
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json({ error: err });
+  });
 });
 
 //Answer part:
-router.route('/:id/answer').post((req, res) => {
-	Question.findById(req.params.id, (err, question) => {
-		console.log(question);
-		const newAnswer = new Answer(req.body.answers);
-		newAnswer.save((err, answer) => {
-			if (err) {
-				res.status(500).json({ 'something is wrong ': err });
-			}
-			question.answers.push(answer);
-			newAnswer.save((err, newAnswer) => {
-				if (err) {
-					return res.status(500).json({ 'something is wrong sec ': err });
-				} else {
-					return res.status(201).json({ newAnswer });
-				}
-			});
-		});
-	});
-});
-
-//Get Question By ID
-router.route('/:questionId').get((req, res) => {
-	const questionId = req.params.questionId;
-	console.log(questionId);
-	Question.findById(req.params.questionId, (err, id) => {
-		console.log(id);
-		console.log('Form database ', id);
-		if (id) {
-			res.status(200).json(id);
-		} else {
-			res.status(400).json('No valid entry found for ID ');
-		}
-	}).catch((err) => {
-		console.log(err);
-		res.status(500).json({ error: err });
-	});
+router.route("/:id/answer").post((req, res) => {
+  Question.findById(req.params.id, (err, question) => {
+    console.log(question);
+    const newAnswer = new Answer(req.body.answers);
+    newAnswer.save((err, answer) => {
+      if (err) {
+        res.status(500).json({ "something is wrong ": err });
+      }
+      question.answers.push(answer);
+      newAnswer.save((err, newAnswer) => {
+        if (err) {
+          return res.status(500).json({ "something is wrong sec ": err });
+        } else {
+          return res
+            .status(201)
+            .json({ question: question, newAnswer: newAnswer });
+        }
+      });
+    });
+  });
 });
 
 // search by title of question
@@ -86,6 +90,5 @@ router.route('/:questionId').get((req, res) => {
 //         res.status(500).json({error: err });
 //     });
 // });
-
 
 module.exports = router;
