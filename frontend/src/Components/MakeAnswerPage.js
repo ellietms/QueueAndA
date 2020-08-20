9cf35958a7b0fa98a56971cd1eeebd5c4684777a
ellie <ellie.tms1371@gmail.com>
2020-08-20 20:19:22 +0100

    back button of answer form page is working with params

--- frontend/src/Components/MakeAnswerPage.js
+++ frontend/src/Components/MakeAnswerPage.js
@@ -3,12 +3,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import AnswerForm from "./AnswerForm";
 import { Link } from "react-router-dom";
 
-const MakeAnswerPage = () => {
+const MakeAnswerPage = (props) => {
   return (
     <div className="mt-2">
       <div>
         <button type="button" className="m-3">
-          <Link to="/showQuestion" style={{ textDecoration: 'none',color: 'black'}}>Back</Link>
+          <Link to={`/questions/${props.match.params.id}`} style={{ textDecoration: 'none',color: 'black'}}>Back</Link>
         </button>
       </div>
       <label className="title_font m-4 pl-5">
