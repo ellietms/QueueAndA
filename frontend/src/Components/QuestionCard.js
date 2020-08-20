b00bc8a1b10dc53ef35ccd3a6baff57d0fad2130
ellie <ellie.tms1371@gmail.com>
2020-08-20 21:51:25 +0100

    Fetching Data for main page is working

--- frontend/src/Components/QuestionCard.js
+++ frontend/src/Components/QuestionCard.js
@@ -1,30 +1,48 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Category from './Category';
-import TitleOfQuestion from './TitleOfQuestion';
-import '../Components/App.css';
-import Question from './Question';
+import Category from "./Category";
+import TitleOfQuestion from "./TitleOfQuestion";
+import "../Components/App.css";
+import Question from "./Question";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const QuestionCard = () => {
-    return(
-        <Link to="/questions/1234" style={{ textDecoration: 'none',color: 'black'}}>  
-        <div className="container main_style mt-4">
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://localhost:5000/questions",
+    })
+      .then((response) => setQuestions(response.data.questions))
+      .catch((error) => console.log(error));
+  }, [questions]);
+
+
+
+  return(
+    questions.map((question, index) => {
+      return (
+        <Link
+          to="/questions/1234"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <div key={index} className="container main_style mt-4">
             <div className="pt-4 pl-3">
-            <Category />
+              <Category category={question.module} />
             </div>
             <div className="d-flex justify-content-between pl-2">
-            <TitleOfQuestion />
+              <TitleOfQuestion title={question.title} />
             </div>
             <div className="pl-3 pb-4">
-            <Question/>
+              <Question question={question.question} />
             </div>
-            <div className="mb-2">
-            </div>
-        </div>
-        </Link> 
-    )
-}
-
+            <div className="mb-2"></div>
+          </div>
+        </Link>
+      )
+      })
+  )
+};
 export default QuestionCard;
-
