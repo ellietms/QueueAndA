03911fe8ee3abed9bc3531813fbb98e151c9ccfd
ellie <ellie.tms1371@gmail.com>
2020-08-26 22:54:31 +0100

    change css

--- frontend/src/Components/AnswerNo.js
+++ frontend/src/Components/AnswerNo.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const AnswerNo = (props) => {
+	console.log(typeof props.answerNo);
+	let NumAnswers;
+	 if(props.answerNo !== 0) {
+	 	NumAnswers = (
+	 		<div className="has_answers d-flex ml-auto">
+	 		<span>Answer {props.answerNo}</span>
+	 	</div>
+	 	)
+	 }
+	 else{
+	 	NumAnswers = (
+			<div className="No_answers d-flex ml-auto">
+	 		<span>Answer {props.answerNo}</span>
+	 	</div>
+	 	)
+	 }
 	return (
-		<ul className="ul_answer">
-			<li>Answer</li>
-			<li>{props.answerNo}</li>
-		</ul>
+		<div>
+			{NumAnswers}
+		</div>
 	);
 };
 
