39a8d4b29fff4dae8d04e60ad4e5283776e9899b
Kyaw <40131560+kiowd@users.noreply.github.com>
2020-08-28 19:49:59 +0100

    added sort function

--- backend/controllers/questions.js
+++ backend/controllers/questions.js
@@ -2,47 +2,50 @@ const Question = require("../models/question.model");
 const Answer = require("../models/answer.model");
 
 module.exports = {
-    
-      allQuestions: (request, response) => {
-        Question.find()
-          .populate("answers")
-          .then(questions => response.json({ questions }))
-          .catch(err => response.send({ Error: err }));
-      },
-      askQuestion : (request, response) => {
-        const newQuestion = new Question(request.body);
-        newQuestion
+  allQuestions: (request, response) => {
+    Question.find()
+      .sort({
+        createdAt: -1,
+      })
+      .populate("answers")
+      .then((questions) => response.json({ questions }))
+      .catch((err) => response.send({ Error: err }));
+  },
+  askQuestion: (request, response) => {
+    const newQuestion = new Question(request.body);
+    newQuestion
+      .save()
+      .then((question) => response.json({ question }))
+      .catch((error) => response.json({ error }));
+  },
+  getQuestionById: (request, response) => {
+    const questionId = request.params.questionId;
+    Question.findById(questionId)
+      .populate("answers")
+      .then((questionWithId) => response.json({ questionWithId }))
+      .catch((error) => {
+        response.status(500).json({ error });
+      });
+  },
+  answerTheSpecificQuestion: (request, response) => {
+    const newAnswer = new Answer(request.body);
+    Question.findById(request.params.id)
+      .then((question) => {
+        newAnswer
           .save()
-          .then(question => response.json({ question }))
-          .catch(error => response.json({ error }));
-      },
-      getQuestionById : (request, response) => {
-        const questionId = request.params.questionId;
-        Question.findById(questionId)
-          .populate("answers")
-          .then(questionWithId => response.json({ questionWithId }))
-          .catch(error => {
-            response.status(500).json({ error });
-          });
-      },
-      answerTheSpecificQuestion : (request, response) => {
-        const newAnswer = new Answer(request.body);
-        Question.findById(request.params.id)
-          .then(question => {
-            newAnswer
+          .then((answer) => {
+            question.answers.push(answer);
+            question.answers.sort((a, b) =>
+              a.createdAt < b.createdAt ? -1 : 1
+            );
+            question
               .save()
-              .then(answer => {
-                question.answers.push(answer);
-                question
-                  .save()
-                  .then(question => response.json({ question, answer }))
-                  .catch(err => response.status(500).json({ Error: err }));
-              })
-              .catch(err => response.status(500).json({ Error: err }));
+              .then((question) => response.json({ question, answer }))
+              .catch((err) => response.status(500).json({ Error: err }));
           })
-      
-          .catch(err => response.status(404).json({ Error: err }));
-      }
+          .catch((err) => response.status(500).json({ Error: err }));
+      })
 
-
+      .catch((err) => response.status(404).json({ Error: err }));
+  }
 };
