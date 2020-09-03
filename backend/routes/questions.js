842b1209aee4ce59139c849ea90c77030cfe1d89
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-09-03 13:18:05 +0100

    this is notificatiion

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Question = require("../models/question.model");
 const Answer = require("../models/answer.model");
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'QueueAndA.uk@gmail.com',
+		pass: 'asdfghjkl1!'
+	}
+});
 
 // all the questions:
 router.route("/").get((request, response) => {
@@ -39,6 +47,19 @@ router.route("/:id/answer").post((request, response) => {
         .save()
         .then(answer => {
           question.answers.push(answer);
+          let mailOptions = {
+            from: 'zarghani.m@gmail.com',
+            to: question.userEmail ,
+            subject: 'Notification From QueueAndA',
+            html: '<div><img src="https://www.newsshopper.co.uk/resources/images/2669419.jpg"/><h1>QueueAndA</h1><br><h2>Hi</h2><h3>your question has been answered by someone.</h3><h3>You can go to the link and get answer. </h3><div><a href="https://cyf-queueanda.netlify.app/">QueueAndA</a></div></div>'
+          };
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
           question
             .save()
             .then(question => response.json({ question, answer }))
