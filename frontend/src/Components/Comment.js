caf713a669ba535dccab82f61f277dc6e67e4eab
hamidreza <hrfarzadeh@gmail.com>
2020-09-09 18:48:37 +0100

    Nothing to be changed for comment just test for some changed in master

---
+++ frontend/src/Components/Comment.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import SubmitButton from './SubmitButton';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+
+function Comment(props) {
+	const [ newComment, setNewComment ] = useState({
+		userName: '',
+		comment: ''
+	});
+
+	function submitHandler(e) {
+		e.preventDefault();
+		axios
+			.post(`http://localhost:5000/answers/${props.answerId}/comment`, newComment)
+			.then((response) => console.log(response))
+			.catch((err) => console.log(err));
+		clearData();
+	}
+	function clearData() {
+		setNewComment({ userName: '', comment: '' });
+		props.displayState(null);
+	}
+
+	const handelForm = (event) => {
+		const commentValue = { ...newComment, [event.target.name]: event.target.value };
+		setNewComment(commentValue);
+	};
+
+	return (
+		<div className="container form_style">
+			<form className="m-5" onSubmit={submitHandler}>
+				<div className="form-group">
+					<label htmlFor="Name" className="mr-auto pt-2">
+						Name:
+					</label>
+					<input
+						type="text"
+						className="form-control"
+						id="Name"
+						onChange={handelForm}
+						value={newComment.userName}
+						name="userName"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="textArea">Comment</label>
+					<textarea
+						className="form-control"
+						rows="5"
+						name="comment"
+						value={newComment.comment}
+						onChange={handelForm}
+					/>
+				</div>
+				<div className="pb-3">
+					<SubmitButton />
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default Comment;
