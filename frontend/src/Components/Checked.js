138835fd299d9a87e3dcd95670216f0b03901e97
ellie <ellie.tms1371@gmail.com>
2020-09-17 16:14:52 +0100

    fixed css issues of question details

--- frontend/src/Components/Checked.js
+++ frontend/src/Components/Checked.js
@@ -4,26 +4,44 @@ import "bootstrap/dist/css/bootstrap.css";
 import Loader from "./Loader";
 
 const Checked = ({ questionDetails }) => {
-  return (
-    <div className="container">
-      {questionDetails ? (
-        <div>
-          {questionDetails.length === 0 ? (
-            <div className="question_needs_answer">
-              This Question Needs &nbsp;Answers{" "}
-            </div>
-          ) : (
-            <div className="question_has_answer">
-              <div className="fas fa-check-square h1 checked rounded" />
-              <span className="pl-2">{questionDetails.length} Answers</span>
-            </div>
-          )}
-        </div>
-      ) : (
+  let answers;
+  if (questionDetails) {
+        if(questionDetails.length === 0)
+        {
+          answers = (
+          <div className="question_needs_answer">
+            This Question Needs &nbsp;Answers{" "}
+          </div>
+          )
+        }
+        else if(questionDetails.length === 1)
+        {
+          answers = (
+          <div className="question_has_answer">
+            <div className="fas fa-check-square h1 checked rounded" />
+            <span className="pl-2">{questionDetails.length} Answer</span>
+          </div>
+          )
+        }
+        else
+        { 
+          answers = (
+          <div className="question_has_answer">
+            <div className="fas fa-check-square h1 checked rounded" />
+            <span className="pl-2">{questionDetails.length} Answers</span>
+          </div>
+          )
+        }
+  } 
+  else {
+    answers = (
+      <div>
         <Loader />
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  return <div className="container">{answers}</div>;
 };
 
 export default Checked;
