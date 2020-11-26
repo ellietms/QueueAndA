75ca1463b64d56d536bde3625b84eaf6ff4a744f
ellie <ellie.tms1371@gmail.com>
2020-11-26 18:19:02 +0000

    interview comments

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -9,71 +9,87 @@ import Pagination from "react-js-pagination";
 import Footer from "./Footer";
 
 const MainPage = () => {
+
   const [questions, setQuestions] = useState([]);
   const [currentQsPage, setCurrentQsPage] = useState(1);
-  const [questionsPerPage] = useState(5);
+  
   const [isLoading, setIsLoading] = useState(false);
 
+  //filtering drop downs and search bar
   const [specificModule, setSpecificModule] = useState("");
   const [searchValue, setSearchValue] = useState("");
   const [noAnswer, setNoAnswer] = useState(false);
+
+  let questionsPerPage = 5;
+
+  //fetch all the questions
   useEffect(() => {
     setIsLoading(true);
     const baseUrl = "https://queueanda.herokuapp.com";
+    
+    //ask johnnie 
     axios
-      .get(`${baseUrl}/questions`,{params:{category:specificModule,searchValue,noAnswer}})
+      .get(`${baseUrl}/questions`, {
+        params: { category: specificModule, searchValue, noAnswer },
+      })
       .then((response) => {
         setQuestions(response.data.questions);
         setIsLoading(false);
         setCurrentQsPage(1);
       })
       .catch((error) => console.log(error));
-  }, [specificModule,searchValue,noAnswer]);
+  }, [specificModule, searchValue, noAnswer]);
 
-  const indexOfLastQs = currentQsPage * questionsPerPage;
-  const indexOfFirstQs = indexOfLastQs - questionsPerPage;
-  const currentQuestions = questions.slice(indexOfFirstQs, indexOfLastQs);
+
+  //pagination
+  const indexOfLastQs = currentQsPage * questionsPerPage; //index of last question in each page  it always is  one more than 
+  const indexOfFirstQs = indexOfLastQs - questionsPerPage; //index of first question in each page
+  const currentQuestions = questions.slice(indexOfFirstQs, indexOfLastQs); // cut questions for each page 
+
 
   const paginate = (pageNumber) => {
     setCurrentQsPage(pageNumber);
   };
 
+  const handleSearchValue = (event) => {
+    event.preventDefault();
+    setSearchValue(event.target.value);
+  }
+
+
   return (
     <div>
       <MainPageHeader
         value={searchValue}
-        handleSearchValue={(newSearchValue) => setSearchValue(newSearchValue)}
         setNoAnswer={setNoAnswer}
         setSpecificModule={setSpecificModule}
+        handleSearchValue = {handleSearchValue}
       />
       {isLoading ? (
         <Loader />
+        //when isLoading is true means it is fetching
       ) : (
         <div>
-        <div  className="container main_page">
-          <div
-            className="container my-auto Questions_amount"
-          >
-            {questions.length} Questions
-          </div>
+          <div className="container main_page">
+            <div className="container my-auto Questions_amount">
+              {questions.length} Questions
+            </div>
 
-          <SelectedQs
-            currentQuestions={currentQuestions}
-          />
+            <SelectedQs currentQuestions={currentQuestions} />
 
-          <div className="pagination">
-            <Pagination
-              prevPageText="prev"
-              nextPageText="next"
-              activePage={currentQsPage}
-              itemsCountPerPage={questionsPerPage}
-              totalItemsCount={questions.length}
-              pageRangeDisplayed={3}
-              onChange={paginate}
-            />
+            <div className="pagination">
+              <Pagination
+                prevPageText="prev"
+                nextPageText="next"
+                activePage={currentQsPage}
+                itemsCountPerPage={questionsPerPage}
+                totalItemsCount={questions.length}
+                pageRangeDisplayed={3}
+                onChange={paginate}
+              />
+            </div>
           </div>
-          </div>
-         <Footer/>
+          <Footer />
         </div>
       )}
     </div>
